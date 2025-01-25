8b2bcb1304bc29d9408fe5718efaf524353e1567
Commander Zoomie <commanderzoomie@gmail.com>
2025-01-25 17:53:35 -0500

    UI
    Menu principal, menu credits

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -5,3 +5,12 @@ const WORLD = preload("res://game/world/world.tscn")
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_packed(WORLD)
+
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/ui/credits_menu.tscn")
