a8b3f6f771c07326452503bb5b625c47aa63fdc5
Josue Ortiz <jotakutsuki@gmail.com>
2025-01-25 17:35:15 -0500

    temp changes

--- game/spike/spike.gd
+++ game/spike/spike.gd
@@ -6,15 +6,17 @@ class_name Spike
 var ran_index
 
 func _ready() -> void:
-	#turn off all spikes
+	set_random_texture()
+	
+
+func _process(delta: float) -> void:
+	pass
+
+func set_random_texture():
 	for col in $StaticBody2D.get_children():
 		col.disabled = true
 		col.visible = false
-	
 	ran_index = randi_range(0, 4)
 	$Sprite2D.texture = sprites[ran_index]
 	$StaticBody2D.get_child(ran_index).disabled = false
 	$StaticBody2D.get_child(ran_index).visible = true
-
-func _process(delta: float) -> void:
-	pass
--- game/world/world.gd
+++ game/world/world.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
-var cur_spikes = []
-var speed = 100
-var tile_width = 420
-var tiles_in_screen_width = 6
+const SPIKE = preload("res://game/spike/spike.tscn")
+
+var cur_spikes:Array[Spike] = []
+@export var speed := 100.0
+@export var tile_width := 420
+@export var tiles_in_screen_width := 6
+
 func _ready():
-	var spike_scene = preload("res://game/spike/spike.tscn")
-	print(spike_scene)
 	for i in range(tiles_in_screen_width):
-		var spike = spike_scene.instantiate()
+		var spike:Spike = SPIKE.instantiate()
 		spike.position.y = 870
 		spike.position.x = tile_width * i
 		add_child(spike)
 		cur_spikes.append(spike)
 	for i in range(tiles_in_screen_width):
-		var spike = spike_scene.instantiate()
+		var spike:Spike = SPIKE.instantiate()
 		spike.position.y = 210
 		spike.position.x = tile_width * i
 		spike.rotation_degrees = 180
@@ -26,5 +27,4 @@ func _process(delta: float) -> void:
 		spike.position.x -= speed * delta
 		if spike.position.x < -tile_width/2:
 			spike.position.x = (tile_width) * tiles_in_screen_width - tile_width/2
-			spike._ready()
-	
+			spike.set_random_texture()
