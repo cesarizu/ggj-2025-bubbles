353abf4e030365e5b5f00d106f8603abea34ba76
Josue Ortiz <jotakutsuki@gmail.com>
2025-01-25 17:23:53 -0500

    added background

--- game/world/world.gd
+++ game/world/world.gd
@@ -1,30 +1,72 @@
 extends Node2D
 
+@onready var background: Sprite2D = $background
+const SPIKE = preload("res://game/spike/spike.tscn")
+const PIPE = preload("res://game/tubo/tubo.tscn")
+
+@export var progress = 0
 var cur_spikes = []
-var speed = 100
-var tile_width = 420
-var tiles_in_screen_width = 6
+var cur_pipes = []
+var speed := 100
+var tile_width := 420
+var tiles_in_screen_width := 6
+var pipe_density := 2.0  #between 0.0 and 10.0
+
 func _ready():
-	var spike_scene = preload("res://game/spike/spike.tscn")
-	print(spike_scene)
+	instanciate_spikes()
+	instanciate_pipes()
+
+func _process(delta: float) -> void:
+	progress += delta
+	move_spikes(delta)
+	move_background(delta)
+	move_pipes(delta)
+
+func move_pipes(delta: float):
+	for pipe in cur_pipes:
+		pipe.position.x -= speed * delta
+		if pipe.position.x < 0:
+			cur_pipes.erase(pipe)
+			pipe.queue_free()
+
+func move_spikes(delta: float):
+	for spike in cur_spikes:
+		spike.position.x -= speed * delta
+		if spike.position.x < -tile_width/2:
+			spike.position.x = (tile_width) * tiles_in_screen_width - tile_width/2
+			spike._ready()
+			
+func move_background(delta: float):
+	background.region_rect.position.x += speed/2 * delta
+
+func instanciate_pipes():
+	for i in range(pipe_density):
+		var pipe = PIPE.instantiate()
+		pipe.position.y = 1080
+		pipe.position.x = 1920/pipe_density * (i + 1) - 100 #100 is bias
+		add_child(pipe)
+		cur_pipes.append(pipe)
+
+func instanciate_spikes():
 	for i in range(tiles_in_screen_width):
-		var spike = spike_scene.instantiate()
+		var spike = SPIKE.instantiate()
 		spike.position.y = 870
 		spike.position.x = tile_width * i
 		add_child(spike)
 		cur_spikes.append(spike)
 	for i in range(tiles_in_screen_width):
-		var spike = spike_scene.instantiate()
+		var spike = SPIKE.instantiate()
 		spike.position.y = 210
 		spike.position.x = tile_width * i
 		spike.rotation_degrees = 180
 		add_child(spike)
 		cur_spikes.append(spike)
 
-func _process(delta: float) -> void:
-	for spike in cur_spikes:
-		spike.position.x -= speed * delta
-		if spike.position.x < -tile_width/2:
-			spike.position.x = (tile_width) * tiles_in_screen_width - tile_width/2
-			spike._ready()
-	
+
+func _on_timer_timeout() -> void:
+	if randf() * 10 < pipe_density:
+		var pipe = PIPE.instantiate()
+		pipe.position.y = 1080
+		pipe.position.x = 2000
+		add_child(pipe)
+		cur_pipes.append(pipe)
