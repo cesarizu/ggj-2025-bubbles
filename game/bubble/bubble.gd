fcd7759f294e806ff74beb27c264975e089e5a7e
César Izurieta <cesar@caih.org>
2025-01-26 14:04:59 -0500

    Make bubbles puncture

--- game/bubble/bubble.gd
+++ game/bubble/bubble.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 @export var grow_factor := 0.01
 @export var shrink_factor := -0.05
+@export var puncture_factor := -0.2
 
 @onready var sprite_2d: Sprite2D = %Sprite2D
 @onready var circle_shape_2d: CircleShape2D = %CollisionShape2D.shape
@@ -11,6 +12,8 @@ extends RigidBody2D
 var _original_sprite_scale: Vector2
 var _original_particles_scale: Vector2
 var _original_radius: float
+
+var _puncturing := false
 
 var bubble_scale := 1.0:
 	set(value):
@@ -37,6 +40,17 @@ func drip_down() -> void:
 	apply_central_impulse(Vector2.DOWN * 25 * mass)
 	scale_bubble(shrink_factor)
 	wobble()
+
+
+func puncture(direction: Vector2) -> void:
+	if _puncturing:
+		return
+
+	_puncturing = true
+	scale_bubble(puncture_factor)
+	apply_central_impulse(direction * 200 * mass)
+	await get_tree().create_timer(1).timeout
+	_puncturing = false
 
 
 func pop() -> void:
--- game/spike/spike.gd
+++ game/spike/spike.gd
@@ -1,8 +1,16 @@
 class_name Spike
 extends Node2D
 
+var _is_upside_down := false
+
 
 func rotate_spike(upside_down: bool) -> void:
+	_is_upside_down = upside_down
 	rotation_degrees = 180 if upside_down else 0
 	%Drop.visible = upside_down
 	%Drop.process_mode = Node.PROCESS_MODE_INHERIT if upside_down else Node.PROCESS_MODE_DISABLED
+
+
+func _on_point_area_2d_body_entered(body: Node2D) -> void:
+	if body is Bubble:
+		body.puncture(Vector2.DOWN if _is_upside_down else Vector2.UP)
