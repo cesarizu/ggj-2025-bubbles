f2028bff5268c8ce7c1639a4242b0904e226b839
César Izurieta <cesar@caih.org>
2025-01-26 10:56:52 -0500

    Reorganized world

--- game/world/world.gd
+++ game/world/world.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-@onready var background: Sprite2D = $background
 const SPIKE = preload("res://game/spike/spike.tscn")
 const PIPE = preload("res://game/tubo/tubo.tscn")
-@export var progress = 0
+
 @export var pipe_density := 0.5  #between 0.0 and 1.0
+
+@onready var background: Sprite2D = $background
+
 var cur_spikes = []
 var cur_pipes = []
 var speed := 100
@@ -13,15 +15,17 @@ var tiles_in_screen_width := 6
 
 var respawn_bias = 0
 
+
 func _ready():
-	instanciate_spikes()
-	instanciate_pipes()
+	instantiate_spikes()
+	instantiate_pipes()
+
 
 func _process(delta: float) -> void:
-	progress += delta
 	move_spikes(delta)
 	move_background(delta)
 	move_pipes(delta)
+
 
 func move_pipes(delta: float):
 	for pipe in cur_pipes:
@@ -30,17 +34,20 @@ func move_pipes(delta: float):
 			cur_pipes.erase(pipe)
 			pipe.queue_free()
 
+
 func move_spikes(delta: float):
 	for spike in cur_spikes:
 		spike.position.x -= speed * delta
 		if spike.position.x < -tile_width/2:
 			spike.position.x = (tile_width) * tiles_in_screen_width - tile_width/2
 			spike._ready()
-			
+
+
 func move_background(delta: float):
 	background.region_rect.position.x += speed/2 * delta
 
-func instanciate_pipes():
+
+func instantiate_pipes():
 	for i in range(3):
 		var pipe = PIPE.instantiate()
 		pipe.position.y = 1080
@@ -48,7 +55,8 @@ func instanciate_pipes():
 		add_child(pipe)
 		cur_pipes.append(pipe)
 
-func instanciate_spikes():
+
+func instantiate_spikes():
 	for i in range(tiles_in_screen_width):
 		var spike = SPIKE.instantiate()
 		spike.position.y = 870
