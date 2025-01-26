d4485839dcb0b6b9c146310ab8831e25d6d7fc4b
Commander Zoomie <commanderzoomie@gmail.com>
2025-01-25 19:22:24 -0500

    UI
    Menus incorporados al sistema del GAME MANAGER

--- game/game_manager.gd
+++ game/game_manager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 const WORLD = preload("res://game/world/world.tscn")
+const MAIN_MENU = preload("res://game/ui/main_menu.tscn")
+const INSTRUCTIONS_MENU = preload("res://game/ui/instructions_menu.tscn")
+const CREDITS_MENU = preload("res://game/ui/credits_menu.tscn")
+const GAME_OVER_MENU = preload("res://game/ui/game_over_menu.tscn")
 
 var level := 1
 
@@ -12,3 +16,16 @@ func new_game() -> void:
 
 func game_over() -> void:
 	pass
+
+
+func main_menu() -> void:
+	get_tree().change_scene_to_packed(MAIN_MENU)
+
+func instructions_menu() -> void:
+	get_tree().change_scene_to_packed(INSTRUCTIONS_MENU)
+
+func credits_menu() -> void:
+	get_tree().change_scene_to_packed(CREDITS_MENU)
+
+func game_over_menu() -> void:
+	get_tree().change_scene_to_packed(GAME_OVER_MENU)
--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -1,16 +1,10 @@
 extends Control
 
-const WORLD = preload("res://game/world/world.tscn")
-
-
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_packed(WORLD)
+	GameManager.new_game()
 
-
+func _on_credits_pressed() -> void:
+	GameManager.credits_menu()
 
 func _on_exit_game_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/ui/credits_menu.tscn")
