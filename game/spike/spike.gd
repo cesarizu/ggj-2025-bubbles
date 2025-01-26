24fc1e4c1be6a864bda6edddcf4c3e9015b0e4a6
jose <josemiguelvasconeza@gmail.com>
2025-01-26 13:56:32 -0500

    Solocuon bug drop.

--- game/spike/spike.gd
+++ game/spike/spike.gd
@@ -5,4 +5,4 @@ extends Node2D
 func rotate_spike(upside_down: bool) -> void:
 	rotation_degrees = 180 if upside_down else 0
 	%Drop.visible = upside_down
-	%Drop.process_mode = Node.PROCESS_MODE_DISABLED if upside_down else Node.PROCESS_MODE_INHERIT
+	%Drop.process_mode = Node.PROCESS_MODE_INHERIT if upside_down else Node.PROCESS_MODE_DISABLED
